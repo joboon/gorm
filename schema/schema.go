5d203a6cb5c1fcf532ffe1346f8e654e0beb4058
John Boone <John.Boone@sas.com>
2024-05-01 11:42:25 -0400

    allow schema field lookups to be case insensitive

--- schema/schema.go
+++ schema/schema.go
@@ -78,9 +78,20 @@ func (schema Schema) LookUpField(name string) *Field {
 	if field, ok := schema.FieldsByDBName[name]; ok {
 		return field
 	}
+	for key, field := range schema.FieldsByDBName {
+		if strings.EqualFold(key, name) {
+			return field
+		}
+	}
 	if field, ok := schema.FieldsByName[name]; ok {
 		return field
 	}
+	for key, field := range schema.FieldsByName {
+		if strings.EqualFold(key, name) {
+			return field
+		}
+	}
+
 	return nil
 }
 
@@ -100,6 +111,11 @@ func (schema Schema) LookUpFieldByBindName(bindNames []string, name string) *Fie
 		find := strings.Join(bindNames[:i], ".") + "." + name
 		if field, ok := schema.FieldsByBindName[find]; ok {
 			return field
+		}
+		for key, field := range schema.FieldsByBindName {
+			if strings.EqualFold(key, find) {
+				return field
+			}
 		}
 	}
 	return nil
